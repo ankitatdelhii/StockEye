32699cad4743bbf330173347134cdb37414e4e27
Ankit Saxena <ankitatdelhii@gmail.com>
2021-07-08 17:57:02 +0530

    ui binding

--- StockEye/controller/SearchTableViewController.swift
+++ StockEye/controller/SearchTableViewController.swift
@@ -10,6 +10,11 @@ import Combine
 
 class SearchTableViewController: UITableViewController {
     
+    private enum Mode {
+        case onboarding
+        case search
+    }
+    
     private lazy var searchController: UISearchController = {
         let sc = UISearchController(searchResultsController: nil)
         sc.searchResultsUpdater = self
@@ -21,18 +26,26 @@ class SearchTableViewController: UITableViewController {
     }()
 
     private let apiService = APIService()
+    private var searchResults: SearchResults?
     private var subscribers = Set<AnyCancellable>()
     @Published private var searchQuery = String()
+    @Published private var mode: Mode = .onboarding
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupNavigationBar()
         observeForm()
+        setupTableView()
     }
 
     private func setupNavigationBar() {
         navigationItem.searchController = searchController
+        navigationItem.title = "Search"
+    }
+    
+    private func setupTableView() {
+        tableView.tableFooterView = UIView()
     }
     
     private func observeForm() {
@@ -48,18 +61,33 @@ class SearchTableViewController: UITableViewController {
                     }
                 } receiveValue: { searchResults in
                     print("Search Results are \(searchResults)")
+                    self.searchResults = searchResults
+                    self.tableView.reloadData()
                 }.store(in: &self.subscribers)
             }.store(in: &subscribers)
+        
+        $mode.sink {[weak self] currentMode in
+            guard let self = self else { return }
+            switch currentMode {
+            case .onboarding:
+                self.tableView.backgroundView = SearchPlaceholderView()
+            case .search:
+                self.tableView.backgroundView = nil
+            }
+        }.store(in: &subscribers)
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath) as! SearchTableViewCell
+        if let hasModel = searchResults?.items[indexPath.row] {
+            cell.confureCell(with: hasModel)
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return searchResults?.items.count ?? 0
     }
 
 }
@@ -69,6 +97,10 @@ extension SearchTableViewController: UISearchResultsUpdating, UISearchController
     func updateSearchResults(for searchController: UISearchController) {
         guard let hasQuery = searchController.searchBar.text, !hasQuery.isEmpty else { return }
         searchQuery = hasQuery
+    }
+    
+    func willPresentSearchController(_ searchController: UISearchController) {
+        mode = .search
     }
 
 }
--- StockEye/view/SearchTableViewCell.swift
+++ StockEye/view/SearchTableViewCell.swift
@@ -13,4 +13,10 @@ class SearchTableViewCell: UITableViewCell {
     @IBOutlet private weak var assetSymbolLabel: UILabel!
     @IBOutlet private weak var assetTypeLabel: UILabel!
     
+    func confureCell(with model: SearchResult) {
+        assetNameLabel.text = model.name
+        assetSymbolLabel.text = model.symbol
+        assetTypeLabel.text = model.type.appending(" ").appending(model.currency)
+    }
+    
 }
